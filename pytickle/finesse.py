4147443dfe9cb03d526d859707eed79f12689b80
Kevin Kuns <kevin.a.kuns@gmail.com>
2019-11-29 17:34:26 -0500

    add verbosity option to tickle in KatFR

--- pytickle/finesse.py
+++ pytickle/finesse.py
@@ -15,14 +15,21 @@ class KatFR:
         self.freqresp = {probe: {} for probe in self.probes}
         self.ff = None
 
-    def tickle(self, fmin, fmax, npts, linlog='log'):
+    def tickle(self, fmin, fmax, npts, linlog='log', verbose=1):
+        if verbose:
+            pbar = tqdm(total=len(self.drives))
+
         for drive in self.drives:
-            print(drive)
+            # make a seperate kat for each drive
             kat = self.kat.deepcopy()
+            if verbose <= 1:
+                kat.verbose = False
+
             kat.signals.f = 1
             kat.add(
                 kcom.xaxis(linlog, [fmin, fmax], kat.signals.f, npts))
 
+            # apply the signal to each detector
             for det in kat.detectors.values():
                 if det.num_demods == 1:
                     det.f1.put(kat.xaxis.x)
@@ -31,12 +38,22 @@ class KatFR:
                 else:
                     raise ValueError('Too many demodulations')
 
+            # run the simulation for this drive
             kat.signals.apply(kat.components[drive].phi, 1, 0)
             kat.parse('yaxis lin re:im')
             kat.parse('scale meter')
             out = kat.run()
+
+            # store the results
             for probe in self.probes:
                 self.freqresp[probe][drive] = out[probe]
+
+            if verbose:
+                pbar.update()
+
+        if verbose:
+            pbar.close()
+
         self.ff = out.x
 
     def getTF(self, probes, drives):
