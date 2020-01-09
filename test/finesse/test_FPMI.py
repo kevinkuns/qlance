f1d14f30501d0b857fd8bf9c75659d9e585a48d0
Kevin Kuns <kevin.a.kuns@gmail.com>
2020-01-08 21:02:25 -0500

    add FPMI unit tests

---
+++ test/finesse/test_FPMI.py
@@ -0,0 +1,161 @@
+"""
+Unit tests for finesse FPMI frequency response and sweeps
+"""
+
+import numpy as np
+import pytickle.finesse as fin
+import pykat
+import pykat.components as kcmp
+import pykat.detectors as kdet
+import pykat.commands as kcmd
+import pytest
+
+
+data = np.load('data/finesse_FPMI_data.npz', allow_pickle=True)
+
+fmod = 55e6
+gmod = 0.1
+Pin = 1
+Ti = 0.1
+Lcav = 40e3
+lm = 3
+ls = 1.36
+lx = lm + ls/2
+ly = lm - ls/2
+dpx = 90
+dpb = 0  # 45 * np.sqrt(2)
+aoib = 45
+Ri = 34e3
+Re = 36e3
+
+
+def katMI(dpx, dpb, aoib):
+    kat = pykat.finesse.kat()
+
+    fin.addMirror(kat, 'EX', Thr=0, phi=dpx, Chr=1/Re)
+    fin.addMirror(kat, 'EY', Thr=0, Chr=1/Re)
+    fin.addMirror(kat, 'IX', Thr=Ti, phi=dpx, Chr=1/Ri)
+    fin.addMirror(kat, 'IY', Thr=Ti, Chr=1/Ri)
+    fin.addBeamSplitter(kat, 'BS', aoi=aoib, phi=dpb)
+
+    kat.add(kcmp.laser('Laser', 'Laser_out', P=Pin))
+    kat.add(kcmp.modulator('Mod', 'Mod_in', 'Mod_out', fmod, gmod, 5, 'pm'))
+    kat.add(kcmp.space('s_Laser_Mod', 'Laser_out', 'Mod_in', 0))
+    kat.add(kcmp.space('s_Mod_BS', 'Mod_out', 'BS_frI', 1))
+
+    kat.add(kcmp.space('s_BS_IX', 'BS_bkT', 'IX_bk', lx))
+    kat.add(kcmp.space('s_IX_EX', 'IX_fr', 'EX_fr', Lcav))
+    fin.setCavityBasis(kat, 'IX_fr', 'EX_fr')
+
+    kat.add(kcmp.space('s_BS_IY', 'BS_frR', 'IY_bk', ly))
+    kat.add(kcmp.space('s_IY_EY', 'IY_fr', 'EY_fr', Lcav))
+    fin.setCavityBasis(kat, 'IY_fr', 'EY_fr')
+
+    kat.add(kcmp.space('s_BS_AS', 'BS_bkO', 'AS_in', 1))
+
+    kat.phase = 2
+
+    return kat
+
+
+class TestFreqResp:
+
+    kat = katMI(90, 0, 45)
+    fin.addReadout(kat, 'AS', 'AS_in', fmod, 0)
+    katTF = fin.KatFR(kat)
+    katTF.tickle(1, 1e4, 400)
+
+    def test_tfQ_EM(self):
+        tfQ_EM = self.katTF.getTF('AS_Q', {'EX': 0.5, 'EY': -0.5})
+        assert np.allclose(tfQ_EM, data['tfQ_EM'])
+
+    def test_tfI_EM(self):
+        tfI_EM = self.katTF.getTF('AS_I', {'EX': 0.5, 'EY': -0.5})
+        assert np.allclose(tfI_EM, data['tfI_EM'])
+
+    def test_tfQ_IM(self):
+        tfQ_IM = self.katTF.getTF('AS_Q', {'IX': 0.5, 'IY': -0.5})
+        assert np.allclose(tfQ_IM, data['tfQ_IM'])
+
+
+def runAsy(ls):
+    kat = katMI(90, 0, 45)
+    kat.verbose = False
+    kat.add(kdet.ad('AS_f0', 0, 'AS_in'))
+    kat.add(kdet.ad('AS_fu', fmod, 'AS_in'))
+    kat.add(kdet.ad('AS_fl', -fmod, 'AS_in'))
+    kat.s_BS_IX.L = lm + ls/2
+    kat.s_BS_IY.L = lm - ls/2
+
+    kat.noxaxis = True
+    return kat.run()
+
+
+class TestSchnupp:
+
+    lasy = np.linspace(0, 3, 100)
+    powf0 = np.zeros_like(lasy)
+    powfu = np.zeros_like(lasy)
+    powfl = np.zeros_like(lasy)
+
+    for li, ls in enumerate(lasy):
+        outAsy = runAsy(ls)
+        powf0[li] = outAsy['AS_f0']**2
+        powfu[li] = outAsy['AS_fu']**2
+        powfl[li] = outAsy['AS_fl']**2
+
+    def test_powf0(self):
+        assert np.allclose(self.powf0, data['powf0'])
+
+    def test_powfu(self):
+        assert np.allclose(self.powfu, data['powfu'])
+
+    def test_powfl(self):
+        assert np.allclose(self.powfl, data['powfl'])
+
+
+def add_ads(kat, name, node):
+    kat.add(kdet.ad(name + '_f0', 0, node))
+    kat.add(kdet.ad(name + '_fu', fmod, node))
+    kat.add(kdet.ad(name + '_fl', -fmod, node))
+
+
+def katSweep(comp, dp=0, verbose=False):
+    kat = katMI(90, 0, 45)
+    kat.verbose = verbose
+    add_ads(kat, 'XARM', 'IX_fr')
+    add_ads(kat, 'YARM', 'IY_fr')
+    add_ads(kat, 'BSX', 'IX_bk')
+    add_ads(kat, 'BSY', 'IY_bk')
+    add_ads(kat, 'AS', 'AS_in')
+
+    kat.add(kcmd.xaxis('lin', np.array(
+        [-180, 180]) - dp, kat.components[comp].phi, 1000))
+    # kat.parse('yaxis re:im')
+    kat.parse('yaxis abs')
+
+    return kat
+
+
+def assert_amp(out, sweep, key):
+    return np.allclose(out[key], data[sweep][()][key])
+
+
+class TestSweep:
+
+    katXARM = katSweep('EX', -90)
+    outXARM = katXARM.run()
+
+    katIX = katSweep('IX', 90)
+    outIX = katIX.run()
+
+    keys = ['AS_f0', 'XARM_f0', 'YARM_f0', 'BSX_f0', 'BSY_f0']
+
+    def test_amps_XARM(self):
+        rslts = [assert_amp(self.outXARM, 'sweep_XARM', key)
+                 for key in self.keys]
+        assert all(rslts)
+
+    def test_amps_IX(self):
+        rslts = [assert_amp(self.outIX, 'sweep_IX', key) for key in self.keys]
+        assert all(rslts)
---
+++ test/optickle/test_FPMI.py
@@ -0,0 +1,149 @@
+"""
+Unit tests for optickle FPMI frequency response and sweeps
+"""
+
+import matlab.engine
+import numpy as np
+import pytickle.optickle as pyt
+import pytest
+
+eng = matlab.engine.start_matlab()
+pyt.addOpticklePath(eng)
+
+
+data = np.load('data/optickle_FPMI_data.npz', allow_pickle=True)
+
+fmod = 55e6
+gmod = 0.1
+vRF = np.array([-fmod, 0, fmod])
+Pin = 1
+Ti = 0.1
+Lcav = 40e3
+lm = 3
+ls = 1.36
+lx = lm + ls/2
+ly = lm - ls/2
+
+
+def optMI(opt_name):
+    opt = pyt.PyTickle(eng, opt_name, vRF)
+
+    opt.addMirror('EX', Thr=0)
+    opt.addMirror('EY', Thr=0)
+    opt.addMirror('IX', Thr=Ti)
+    opt.addMirror('IY', Thr=Ti)
+    opt.addBeamSplitter('BS', aoi=45, Thr=0.5)
+
+    opt.addSource('Laser', np.sqrt(Pin)*(vRF == 0))
+    opt.addRFmodulator('Mod', fmod, gmod*1j)
+    opt.addLink('Laser', 'out', 'Mod', 'in', 0)
+    opt.addLink('Mod', 'out', 'BS', 'frA', 1)
+
+    opt.addLink('BS', 'bkA', 'IX', 'bk', lx)
+    opt.addLink('IX', 'fr', 'EX', 'fr', Lcav)
+    opt.addLink('EX', 'fr', 'IX', 'fr', Lcav)
+    opt.addLink('IX', 'bk', 'BS', 'bkB', lx)
+
+    opt.addLink('BS', 'frA', 'IY', 'bk', ly)
+    opt.addLink('IY', 'fr', 'EY', 'fr', Lcav)
+    opt.addLink('EY', 'fr', 'IY', 'fr', Lcav)
+    opt.addLink('IY', 'bk', 'BS', 'frB', ly)
+
+    opt.addSink('AS')
+    opt.addLink('BS', 'bkB', 'AS', 'in', 1)
+    opt.addReadout('AS', fmod, 0)
+
+    return opt
+
+
+class TestFreqResp:
+
+    opt = optMI('optFR')
+    ff = np.logspace(0, 4, 400)
+    opt.tickle(ff, noise=False)
+
+    def test_tfQ_EM(self):
+        tfQ_EM = self.opt.getTF('AS_Q', {'EX': 0.5, 'EY': -0.5})
+        assert np.allclose(tfQ_EM, data['tfQ_EM'])
+
+    def test_tfI_EM(self):
+        tfI_EM = self.opt.getTF('AS_I', {'EX': 0.5, 'EY': -0.5})
+        assert np.allclose(tfI_EM, data['tfI_EM'])
+
+    def test_tfQ_IM(self):
+        tfQ_IM = self.opt.getTF('AS_Q', {'IX': 0.5, 'IY': -0.5})
+        assert np.allclose(tfQ_IM, data['tfQ_IM'])
+
+
+class TestSchnupp:
+    lmean = (lx + ly)/2
+    lasy = np.linspace(0, 3, 100)
+    lxx = lmean + lasy/2
+    lyy = lmean - lasy/2
+
+    optAsy = optMI('optAsy')
+
+    powf0 = np.zeros_like(lasy)
+    powfu = np.zeros_like(lasy)
+    powfl = np.zeros_like(lasy)
+
+    for ii in range(len(lasy)):
+        optAsy.setLinkLength('BS', 'IX', lxx[ii])
+        optAsy.setLinkLength('IX', 'BS', lxx[ii])
+        optAsy.setLinkLength('BS', 'IY', lyy[ii])
+        optAsy.setLinkLength('IY', 'BS', lyy[ii])
+
+        optAsy.tickle(1, noise=False)
+        powf0[ii] = optAsy.getDCpower('BS', 'AS', 0)
+        powfu[ii] = optAsy.getDCpower('BS', 'AS', fmod)
+        powfl[ii] = optAsy.getDCpower('BS', 'AS', -fmod)
+
+    def test_powf0(self):
+        assert np.allclose(self.powf0, data['powf0'])
+
+    def test_powfu(self):
+        assert np.allclose(self.powfu, data['powfu'])
+
+    def test_powfl(self):
+        assert np.allclose(self.powfl, data['powfl'])
+
+
+def computeSweep(drive):
+    optSweep = optMI('optSweep')
+    ePos = {drive: 180 * optSweep.lambda0[0]/360}
+    sPos = {k: -v for k, v in ePos.items()}
+    optSweep.sweepLinear(sPos, ePos, 1000)
+    return optSweep
+
+
+def getSweepPowers(opt, drive):
+    locs = {'XARM': ('IX', 'EX'),
+            'YARM': ('IY', 'EY'),
+            'BSX': ('BS', 'IX'),
+            'BSY': ('BS', 'IY'),
+            'AS': ('BS', 'AS')}
+    # locs = ['XARM', 'YARM', 'BSX', 'BSY', 'AS']
+    powers = {loc: {} for loc in locs.keys()}
+    poses = {}
+    for loc, (sopt, eopt) in locs.items():
+        poses[loc], powers[loc]['f0'] = opt.getSweepPower(drive, sopt, eopt, 0)
+        _, powers[loc]['fu'] = opt.getSweepPower(drive, sopt, eopt, fmod)
+        _, powers[loc]['fl'] = opt.getSweepPower(drive, sopt, eopt, -fmod)
+
+    return poses, powers
+
+
+def assert_amp(optic, powers_ref):
+    opt = computeSweep(optic)
+    _, powers = getSweepPowers(opt, optic)
+    rslt = [np.allclose(powers[key]['f0'], powers_ref[key]['f0'])
+            for key in powers.keys()]
+    return rslt
+
+
+def test_amps_XARM():
+    assert all(assert_amp('EX', data['powersXARM'][()]))
+
+
+def test_amps_IX():
+    assert all(assert_amp('IX', data['powersIX'][()]))
