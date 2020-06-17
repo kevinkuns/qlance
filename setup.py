51bec685f89a6f8dd7b33a21688c197c62b638c1
Kevin Kuns <kevin.a.kuns@gmail.com>
2020-06-17 02:10:47 -0400

    add setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+"""
+Setup
+"""
+
+import setuptools
+
+version = '0.1.0'
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='PyTickle',
+    version=version,
+    url='https://github.com/kevinkuns/pytickle',
+    author='Kevin Kuns',
+    author_email='kkuns@mit.edu',
+    description='Optomechanical, quantum noise, and control loop simulations',
+    long_description=long_description,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent'
+    ]
+)
